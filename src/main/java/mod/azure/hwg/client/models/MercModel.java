769ba129ef2176eb30e1558bcaa36bcacfefc94b
AzureZhen <rcraft@cyberconnects2.com>
2021-02-28 12:56:26 -0500

    clean up and attempt at something

--- src/main/java/mod/azure/hwg/client/models/MercModel.java
+++ src/main/java/mod/azure/hwg/client/models/MercModel.java
@@ -28,15 +28,25 @@ public class MercModel extends AnimatedGeoModel<MercEntity> {
 		return new Identifier(HWGMod.MODID, "animations/merc_illager.animation.json");
 	}
 
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
 	public void setLivingAnimations(MercEntity entity, Integer uniqueID, AnimationEvent customPredicate) {
 		super.setLivingAnimations(entity, uniqueID, customPredicate);
 		IBone head = this.getAnimationProcessor().getBone("head");
+
+//        IBone Left = this.getAnimationProcessor().getBone("BipedLeftLeg");
+//        IBone Right = this.getAnimationProcessor().getBone("BipedRightLeg");
 
 		EntityModelData extraData = (EntityModelData) customPredicate.getExtraDataOfType(EntityModelData.class).get(0);
 		if (head != null) {
 			head.setRotationX(extraData.headPitch * ((float) Math.PI / 180F));
 			head.setRotationY(extraData.netHeadYaw * ((float) Math.PI / 180F));
 		}
+//		if (Left != null) {
+//			Left.setRotationX(MathHelper.cos(entity.limbAngle * 0.6662F + 3.1415927F) * 1.4F * entity.limbDistance * 0.5F);
+//		}
+//		if (Right != null) {
+//			Right.setRotationX(MathHelper.cos(entity.limbAngle * 0.6662F) * 1.4F * entity.limbDistance * 0.5F);
+//		}
 	}
 }
