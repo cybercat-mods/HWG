2f2ee32233f8d6ef19b454eb6b3541fe748ba1df
AzureZhen <rcraft@cyberconnects2.com>
2021-04-15 23:01:42 -0400

    v1.0.10

--- src/main/java/mod/azure/hwg/util/MobSpawn.java
+++ src/main/java/mod/azure/hwg/util/MobSpawn.java
@@ -63,41 +63,6 @@ public class MobSpawn {
 						config.greater_spawn_weight, config.greater_min_group, config.greater_max_group));
 			}
 		}
-//		BiomeModifications.addSpawn(
-//				BiomeSelectors.foundInOverworld()
-//						.and(context -> !context.getBiome().getSpawnSettings()
-//								.getSpawnEntry(HWGMobs.MERC.getSpawnGroup()).isEmpty()
-//								&& (context.getBiome().getCategory() == Biome.Category.FOREST
-//										|| context.getBiome().getCategory() == Biome.Category.TAIGA
-//										|| context.getBiome().getCategory() == Biome.Category.DESERT
-//										|| context.getBiome().getCategory() == Biome.Category.MESA
-//										|| context.getBiome().getCategory() == Biome.Category.JUNGLE
-//										|| context.getBiome().getCategory() == Biome.Category.PLAINS
-//										|| context.getBiome().getCategory() == Biome.Category.ICY)),
-//				SpawnGroup.MONSTER, HWGMobs.MERC, config.merc_spawn_weight, config.merc_min_group,
-//				config.merc_max_group);
-//		BiomeModifications.addSpawn(
-//				BiomeSelectors.foundInOverworld()
-//						.and(context -> !context.getBiome().getSpawnSettings()
-//								.getSpawnEntry(HWGMobs.SPY.getSpawnGroup()).isEmpty()
-//								&& context.getBiome().getCategory() == Biome.Category.TAIGA
-//								|| context.getBiome().getCategory() == Biome.Category.MESA
-//								|| context.getBiome().getCategory() == Biome.Category.JUNGLE),
-//				SpawnGroup.MONSTER, HWGMobs.SPY, config.spy_spawn_weight, config.spy_min_group, config.spy_max_group);
-//		BiomeModifications.addSpawn(
-//				BiomeSelectors.foundInTheNether()
-//						.and(context -> !context.getBiome().getSpawnSettings()
-//								.getSpawnEntry(HWGMobs.TECHNOLESSER.getSpawnGroup()).isEmpty()
-//								&& context.getBiome().getCategory() == Biome.Category.NETHER),
-//				SpawnGroup.MONSTER, HWGMobs.TECHNOLESSER, config.lesser_spawn_weight, config.lesser_min_group,
-//				config.lesser_max_group);
-//		BiomeModifications.addSpawn(
-//				BiomeSelectors.foundInTheNether()
-//						.and(context -> !context.getBiome().getSpawnSettings()
-//								.getSpawnEntry(HWGMobs.TECHNOGREATER.getSpawnGroup()).isEmpty()
-//								&& context.getBiome().getCategory() == Biome.Category.NETHER),
-//				SpawnGroup.MONSTER, HWGMobs.TECHNOGREATER, config.greater_spawn_weight, config.greater_min_group,
-//				config.greater_max_group);
 	}
 
 	public static void addMobSpawnToBiome(Biome biome, SpawnGroup classification,
