3481f49ed70d0936e8c4fdefafd5a5035545a633
AzureZhen <rcraft@cyberconnects2.com>
2021-07-06 22:18:48 -0400

    v1.0.17

--- src/main/java/mod/azure/hwg/blocks/GunTableBlock.java
+++ src/main/java/mod/azure/hwg/blocks/GunTableBlock.java
@@ -12,6 +12,8 @@ import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.state.StateManager;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.Hand;
 import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.hit.BlockHitResult;
@@ -24,16 +26,18 @@ import net.minecraft.world.World;
 
 public class GunTableBlock extends HorizontalFacingBlock implements BlockEntityProvider {
 
+	private static final VoxelShape X_LENGTH1 = Block.createCuboidShape(0, 0, 1, 16, 17, 16);
+	private static final VoxelShape X_LENGTH2 = Block.createCuboidShape(0, 16, 14, 16, 29, 15);
+	private static final VoxelShape Y_LENGTH1 = Block.createCuboidShape(1, 0, 0, 16, 17, 16);
+	private static final VoxelShape Y_LENGTH2 = Block.createCuboidShape(14, 16, 0, 15, 29, 16);
+	private static final VoxelShape X_AXIS_AABB = VoxelShapes.union(X_LENGTH1, X_LENGTH2);
+	private static final VoxelShape Z_AXIS_AABB = VoxelShapes.union(Y_LENGTH1, Y_LENGTH2);
+	
 	public GunTableBlock(Settings settings) {
 		super(settings);
 		this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH));
 	}
-
-	@Override
-	public BlockState getPlacementState(ItemPlacementContext ctx) {
-		return this.getDefaultState().with(FACING, ctx.getPlayerFacing());
-	}
-
+	
 	@Override
 	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
 		builder.add(FACING);
@@ -83,9 +87,22 @@ public class GunTableBlock extends HorizontalFacingBlock implements BlockEntityP
 		return ScreenHandler.calculateComparatorOutput(world.getBlockEntity(pos));
 	}
 
+	public BlockState rotate(BlockState state, BlockRotation rotation) {
+		return (BlockState) state.with(FACING, rotation.rotate((Direction) state.get(FACING)));
+	}
+
+	public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+		Direction direction = (Direction) state.get(FACING);
+		return direction.getAxis() == Direction.Axis.X ? Z_AXIS_AABB : X_AXIS_AABB;
+	}
+
+	public BlockState mirror(BlockState state, BlockMirror mirror) {
+		return state.rotate(mirror.getRotation((Direction) state.get(FACING)));
+	}
+
 	@Override
-	public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
-		return VoxelShapes.cuboid(0.06f, 0.0f, 0.0f, 0.88f, 1.0f, 1.00f);
+	public BlockState getPlacementState(ItemPlacementContext context) {
+		return this.getDefaultState().with(FACING, context.getPlayerLookDirection());
 	}
 
 }
