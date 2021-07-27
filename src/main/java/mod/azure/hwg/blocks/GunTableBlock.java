038804ea96d16c8da097ba29c1ae984d13ebb7db
AzureZhen <rcraft@cyberconnects2.com>
2021-07-26 23:40:47 -0400

    Closes #26

--- src/main/java/mod/azure/hwg/blocks/GunTableBlock.java
+++ src/main/java/mod/azure/hwg/blocks/GunTableBlock.java
@@ -3,7 +3,6 @@ package mod.azure.hwg.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -11,6 +10,8 @@ import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.BlockMirror;
 import net.minecraft.util.BlockRotation;
@@ -24,15 +25,16 @@ import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
-public class GunTableBlock extends HorizontalFacingBlock implements BlockEntityProvider {
+public class GunTableBlock extends Block implements BlockEntityProvider {
 
-	private static final VoxelShape X_LENGTH1 = Block.createCuboidShape(0, 0, 1, 16, 17, 16); 
-	private static final VoxelShape X_LENGTH2 = Block.createCuboidShape(0, 16, 14, 16, 29, 15); 
-	private static final VoxelShape Y_LENGTH1 = Block.createCuboidShape(1, 0, 0, 16, 17, 16); 
-	private static final VoxelShape Y_LENGTH2 = Block.createCuboidShape(14, 16, 0, 15, 29, 16); 
+	public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
+	private static final VoxelShape X_LENGTH1 = Block.createCuboidShape(0, 0, 1, 16, 17, 16);
+	private static final VoxelShape X_LENGTH2 = Block.createCuboidShape(0, 16, 14, 16, 29, 15);
+	private static final VoxelShape Y_LENGTH1 = Block.createCuboidShape(1, 0, 0, 16, 17, 16);
+	private static final VoxelShape Y_LENGTH2 = Block.createCuboidShape(14, 16, 0, 15, 29, 16);
 	private static final VoxelShape X_AXIS_AABB = VoxelShapes.union(X_LENGTH1, X_LENGTH2);
 	private static final VoxelShape Z_AXIS_AABB = VoxelShapes.union(Y_LENGTH1, Y_LENGTH2);
-	
+
 	public GunTableBlock(Settings settings) {
 		super(settings);
 		this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH));
@@ -41,6 +43,24 @@ public class GunTableBlock extends HorizontalFacingBlock implements BlockEntityP
 	@Override
 	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
 		builder.add(FACING);
+	}
+
+	public BlockState rotate(BlockState state, BlockRotation rotation) {
+		return (BlockState) state.with(FACING, rotation.rotate((Direction) state.get(FACING)));
+	}
+
+	public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+		Direction direction = (Direction) state.get(FACING);
+		return direction.getAxis() == Direction.Axis.X ? Z_AXIS_AABB : X_AXIS_AABB;
+	}
+
+	public BlockState mirror(BlockState state, BlockMirror mirror) {
+		return state.rotate(mirror.getRotation((Direction) state.get(FACING)));
+	}
+
+	@Override
+	public BlockState getPlacementState(ItemPlacementContext context) {
+		return this.getDefaultState().with(FACING, context.getPlayerFacing());
 	}
 
 	@Override
@@ -85,24 +105,6 @@ public class GunTableBlock extends HorizontalFacingBlock implements BlockEntityP
 	@Override
 	public int getComparatorOutput(BlockState state, World world, BlockPos pos) {
 		return ScreenHandler.calculateComparatorOutput(world.getBlockEntity(pos));
-	}
-
-	public BlockState rotate(BlockState state, BlockRotation rotation) {
-		return (BlockState) state.with(FACING, rotation.rotate((Direction) state.get(FACING)));
-	}
-
-	public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-		Direction direction = (Direction) state.get(FACING);
-		return direction.getAxis() == Direction.Axis.X ? Z_AXIS_AABB : X_AXIS_AABB;
-	}
-
-	public BlockState mirror(BlockState state, BlockMirror mirror) {
-		return state.rotate(mirror.getRotation((Direction) state.get(FACING)));
-	}
-
-	@Override
-	public BlockState getPlacementState(ItemPlacementContext context) {
-		return this.getDefaultState().with(FACING, context.getPlayerLookDirection());
 	}
 
 }
