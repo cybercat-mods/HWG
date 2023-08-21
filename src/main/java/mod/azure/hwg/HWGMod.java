fa006853373b81f733856a5844fcba134014aec2
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-08-20 21:06:14 -0400

    Spawn Configs to Spawn Tags

--- src/main/java/mod/azure/hwg/HWGMod.java
+++ src/main/java/mod/azure/hwg/HWGMod.java
@@ -29,11 +29,13 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
@@ -50,6 +52,7 @@ public class HWGMod implements ModInitializer {
 	public static HWGParticles PARTICLES;
 	public static final String MODID = "hwg";
 	public static ProjectilesEntityRegister PROJECTILES;
+	public static MenuType<GunTableScreenHandler> SCREEN_HANDLER_TYPE;
 	public static final ResourceLocation LUGER = new ResourceLocation(MODID, "luger");
 	public static final ResourceLocation HELL = new ResourceLocation(MODID, "hellgun");
 	public static final ResourceLocation ASSASULT = new ResourceLocation(MODID, "smg");
@@ -72,7 +75,10 @@ public class HWGMod implements ModInitializer {
 	public static final ResourceLocation GUNSMITH_POI = new ResourceLocation(MODID, "gun_smith_poi");
 	public static final ResourceLocation GUN_TABLE_GUI = new ResourceLocation(MODID, "gun_table_gui");
 	public static final ResourceLocation ROCKETLAUNCHER = new ResourceLocation(MODID, "rocketlauncher");
-	public static MenuType<GunTableScreenHandler> SCREEN_HANDLER_TYPE;
+	public static final TagKey<Biome> SPY_BIOMES = TagKey.create(Registries.BIOME, HWGMod.modResource("spy_biomes"));
+	public static final TagKey<Biome> MERC_BIOMES = TagKey.create(Registries.BIOME, HWGMod.modResource("merc_biomes"));
+	public static final TagKey<Biome> TECHNOLESSER_BIOMES = TagKey.create(Registries.BIOME, HWGMod.modResource("technolesser_biomes"));
+	public static final TagKey<Biome> TECHNOGREATER_BIOMES = TagKey.create(Registries.BIOME, HWGMod.modResource("technogreater_biomes"));
 	public static final ResourceKey<CreativeModeTab> WeaponItemGroup = ResourceKey.create(Registries.CREATIVE_MODE_TAB, new ResourceLocation(MODID, "weapons"));
 	public static final RecipeSerializer<GunTableRecipe> GUN_TABLE_RECIPE_SERIALIZER = Registry.register(BuiltInRegistries.RECIPE_SERIALIZER, new ResourceLocation(MODID, "gun_table"), new GunTableRecipe.Serializer());
 
@@ -166,5 +172,9 @@ public class HWGMod implements ModInitializer {
 		SCREEN_HANDLER_TYPE = new MenuType<>(GunTableScreenHandler::new, FeatureFlags.VANILLA_SET);
 		Registry.register(BuiltInRegistries.MENU, new ResourceLocation(MODID, "guntable_screen_type"), SCREEN_HANDLER_TYPE);
 		PacketHandler.registerMessages();
+	}
+
+	public static final ResourceLocation modResource(String name) {
+		return new ResourceLocation(MODID, name);
 	}
 }
--- src/main/java/mod/azure/hwg/util/MobSpawn.java
+++ src/main/java/mod/azure/hwg/util/MobSpawn.java
@@ -1,8 +1,5 @@
 package mod.azure.hwg.util;
 
-import java.util.Arrays;
-import java.util.List;
-
 import mod.azure.hwg.HWGMod;
 import mod.azure.hwg.entity.MercEntity;
 import mod.azure.hwg.entity.SpyEntity;
@@ -12,20 +9,22 @@ import mod.azure.hwg.util.registry.HWGMobs;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectionContext;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.levelgen.Heightmap;
 
 public class MobSpawn {
 
 	public static void addSpawnEntries() {
-		BiomeModifications.addSpawn(BiomeSelectors.foundInOverworld().and(context -> parseBiomes(HWGMod.config.merc_biomes, context)), MobCategory.MONSTER, HWGMobs.MERC, HWGMod.config.merc_spawn_weight, HWGMod.config.merc_min_group, HWGMod.config.merc_max_group);
+		BiomeModifications.addSpawn(BiomeSelectors.all().and(context -> parseBiomes(HWGMod.MERC_BIOMES, context)), MobCategory.MONSTER, HWGMobs.MERC, HWGMod.config.merc_spawn_weight, HWGMod.config.merc_min_group, HWGMod.config.merc_max_group);
 
-		BiomeModifications.addSpawn(BiomeSelectors.foundInOverworld().and(context -> parseBiomes(HWGMod.config.spy_biomes, context)), MobCategory.MONSTER, HWGMobs.SPY, HWGMod.config.spy_spawn_weight, HWGMod.config.spy_min_group, HWGMod.config.spy_max_group);
+		BiomeModifications.addSpawn(BiomeSelectors.all().and(context -> parseBiomes(HWGMod.SPY_BIOMES, context)), MobCategory.MONSTER, HWGMobs.SPY, HWGMod.config.spy_spawn_weight, HWGMod.config.spy_min_group, HWGMod.config.spy_max_group);
 
-		BiomeModifications.addSpawn(BiomeSelectors.all().and(context -> parseBiomes(HWGMod.config.lesser_biomes, context)), MobCategory.MONSTER, HWGMobs.TECHNOLESSER, HWGMod.config.lesser_spawn_weight, HWGMod.config.lesser_min_group, HWGMod.config.lesser_max_group);
+		BiomeModifications.addSpawn(BiomeSelectors.all().and(context -> parseBiomes(HWGMod.TECHNOLESSER_BIOMES, context)), MobCategory.MONSTER, HWGMobs.TECHNOLESSER, HWGMod.config.lesser_spawn_weight, HWGMod.config.lesser_min_group, HWGMod.config.lesser_max_group);
 
-		BiomeModifications.addSpawn(BiomeSelectors.all().and(context -> parseBiomes(HWGMod.config.greater_biomes, context)), MobCategory.MONSTER, HWGMobs.TECHNOGREATER, HWGMod.config.greater_spawn_weight, HWGMod.config.greater_min_group, HWGMod.config.greater_max_group);
+		BiomeModifications.addSpawn(BiomeSelectors.all().and(context -> parseBiomes(HWGMod.TECHNOGREATER_BIOMES, context)), MobCategory.MONSTER, HWGMobs.TECHNOGREATER, HWGMod.config.greater_spawn_weight, HWGMod.config.greater_min_group, HWGMod.config.greater_max_group);
 
 		SpawnPlacements.register(HWGMobs.TECHNOLESSER, SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, TechnodemonEntity::canNetherSpawn);
 		SpawnPlacements.register(HWGMobs.TECHNOGREATER, SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, TechnodemonGreaterEntity::canNetherSpawn);
@@ -33,8 +32,7 @@ public class MobSpawn {
 		SpawnPlacements.register(HWGMobs.SPY, SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, SpyEntity::canSpawn);
 	}
 
-	private static boolean parseBiomes(String[] biomes, BiomeSelectionContext biomeContext) {
-		List<String> biomelist = Arrays.asList(biomes);
-		return biomelist.contains(biomeContext.getBiomeKey().location().toString());
+	private static boolean parseBiomes(TagKey<Biome> biomes, BiomeSelectionContext biomeContext) {
+		return biomeContext.hasTag(biomes);
 	}
 }
