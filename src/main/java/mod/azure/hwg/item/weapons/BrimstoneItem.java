1866157e5dc113eb05f29116128e230ce96af2f7
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-11-09 13:47:00 -0500

    change brimstone reload sound

--- src/main/java/mod/azure/hwg/item/weapons/BrimstoneItem.java
+++ src/main/java/mod/azure/hwg/item/weapons/BrimstoneItem.java
@@ -87,7 +87,7 @@ public class BrimstoneItem extends HWGGunBase {
                 removeAmmo(HWGItems.FUEL_TANK, user);
                 user.getItemInHand(hand).hurtAndBreak(-HWGMod.config.gunconfigs.brimstoneconfigs.brimstone_cap, user, s -> user.broadcastBreakEvent(hand));
                 user.getItemInHand(hand).setPopTime(3);
-                user.level().playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.FIRECHARGE_USE, SoundSource.PLAYERS, 1.0F, 1.5F);
+                user.level().playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ANVIL_FALL, SoundSource.PLAYERS, 1.0F, 1.5F);
             }
         }
     }
