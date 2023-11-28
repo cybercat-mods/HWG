e2b86d7996e2e89467839975ee3fd924b63154ca
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-11-28 17:38:04 -0500

    Fixes shooter being on fire

--- src/main/java/mod/azure/hwg/util/Helper.java
+++ src/main/java/mod/azure/hwg/util/Helper.java
@@ -5,32 +5,30 @@ import mod.azure.azurelib.platform.Services;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
 public class Helper {
     private static BlockPos lightBlockPos = null;
+
     public static void setOnFire(Entity projectile) {
         if (projectile.isOnFire())
             projectile.level().getEntitiesOfClass(LivingEntity.class, projectile.getBoundingBox().inflate(2)).forEach(e -> {
-                if (e.isAlive())
-                    e.setRemainingFireTicks(90);
+                if (e.isAlive() && !(e instanceof Player)) e.setRemainingFireTicks(90);
             });
     }
 
     public static void spawnLightSource(Entity entity, Boolean isInWaterBlock) {
         if (lightBlockPos == null) {
             lightBlockPos = findFreeSpace(entity.level(), entity.blockPosition(), 2);
-            if (lightBlockPos == null)
-                return;
+            if (lightBlockPos == null) return;
             entity.level().setBlockAndUpdate(lightBlockPos, Services.PLATFORM.getTickingLightBlock().defaultBlockState());
         } else if (checkDistance(lightBlockPos, entity.blockPosition(), 2)) {
             var blockEntity = entity.level().getBlockEntity(lightBlockPos);
             if (blockEntity instanceof TickingLightEntity tickingLightEntity)
                 tickingLightEntity.refresh(isInWaterBlock ? 20 : 0);
-            else
-                lightBlockPos = null;
-        } else
-            lightBlockPos = null;
+            else lightBlockPos = null;
+        } else lightBlockPos = null;
     }
 
     private static boolean checkDistance(BlockPos blockPosA, BlockPos blockPosB, int distance) {
@@ -38,8 +36,7 @@ public class Helper {
     }
 
     private static BlockPos findFreeSpace(Level world, BlockPos blockPos, int maxDistance) {
-        if (blockPos == null)
-            return null;
+        if (blockPos == null) return null;
 
         var offsets = new int[maxDistance * 2 + 1];
         offsets[0] = 0;
