19aebe4fae9964c7359b251ba28ddbe2ca44df74
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-09-11 14:05:58 -0400

    Make Fabric to NeoForge changes

--- common/src/main/java/mod/azure/hwg/util/GunSmithProfession.java
+++ common/src/main/java/mod/azure/hwg/util/GunSmithProfession.java
@@ -22,7 +22,27 @@ import java.util.Optional;
 public record GunSmithProfession() {
 
     public static void init() {
-        VillagerTrades.TRADES.put(HWGProfession.GUNSMITH.get(), copyToFastUtilMap(ImmutableMap.of(1, new VillagerTrades.ItemListing[]{new BuyForOneEmeraldFactory(Items.GUNPOWDER, 1, 16, 2), new SellItemFactory(Items.IRON_NUGGET, 2, 1, 16, 1)}, 2, new VillagerTrades.ItemListing[]{new BuyForItemFactory(Items.EMERALD, HWGItems.BULLETS.get(), 2, 16, 10), new BuyForItemFactory(Items.EMERALD, HWGItems.PISTOL.get(), 5, 16, 20), new BuyForItemFactory(Items.EMERALD, HWGItems.LUGER.get(), 5, 16, 20)}, 3, new VillagerTrades.ItemListing[]{new BuyForItemsFactory(Items.EMERALD, 2, 1, HWGItems.SHOTGUN_SHELL.get(), 16, 16, 30), new BuyForItemsFactory(Items.IRON_INGOT, 3, HWGItems.SMG.get(), 1, 16, 30), new BuyForItemsFactory(Items.IRON_INGOT, 3, HWGItems.TOMMYGUN.get(), 1, 16, 30)}, 4, new VillagerTrades.ItemListing[]{new BuyForItemsFactory(HWGItems.FUEL_TANK.get(), 1, 4, HWGItems.FLAMETHROWER.get(), 1, 16, 40), new BuyForItemsFactory(Items.IRON_INGOT, 6, 4, HWGItems.SHOTGUN.get(), 1, 16, 40), new BuyForItemsFactory(Items.GUNPOWDER, 8, 4, HWGItems.BULLETS.get(), 48, 16, 50)}, 5, new VillagerTrades.ItemListing[]{new BuyForItemsFactory(Items.IRON_INGOT, 18, 8, HWGItems.ROCKETLAUNCHER.get(), 1, 16, 60), new BuyForItemsFactory(Items.IRON_INGOT, 18, 8, HWGItems.G_LAUNCHER.get(), 1, 16, 60), new BuyForItemsFactory(Items.IRON_INGOT, 18, 8, HWGItems.SNIPER.get(), 1, 16, 60)})));
+        VillagerTrades.TRADES.put(HWGProfession.GUNSMITH.get(), copyToFastUtilMap(ImmutableMap.of(
+                1, new VillagerTrades.ItemListing[]{
+                        new VillagerTrades.EmeraldForItems(Items.GUNPOWDER, 1, 16, 2),
+                        new VillagerTrades.ItemsForEmeralds(Items.IRON_NUGGET, 2, 1, 16, 1)},
+                2, new VillagerTrades.ItemListing[]{
+                        new VillagerTrades.EmeraldForItems(HWGItems.BULLETS.get(), 2, 16, 10),
+                        new VillagerTrades.EmeraldForItems(HWGItems.PISTOL.get(), 5, 16, 20),
+                        new VillagerTrades.EmeraldForItems(HWGItems.LUGER.get(), 5, 16, 20)},
+                3, new VillagerTrades.ItemListing[]{
+                        new VillagerTrades.EmeraldForItems(HWGItems.SHOTGUN_SHELL.get(), 16, 16, 30),
+                        new VillagerTrades.ItemsAndEmeraldsToItems(Items.IRON_INGOT, 3, 1, HWGItems.SMG.get(), 1, 16, 30, 0.05F),
+                        new VillagerTrades.ItemsAndEmeraldsToItems(Items.IRON_INGOT, 3, 1, HWGItems.TOMMYGUN.get(), 1, 16, 30, 0.05F)},
+                4, new VillagerTrades.ItemListing[]{
+                        new VillagerTrades.ItemsAndEmeraldsToItems(HWGItems.FUEL_TANK.get(), 1, 4, HWGItems.FLAMETHROWER.get(), 1, 16, 40, 0.05F),
+                        new VillagerTrades.ItemsAndEmeraldsToItems(Items.IRON_INGOT, 6, 4, HWGItems.SHOTGUN.get(), 1, 16, 40, 0.05F),
+                        new VillagerTrades.ItemsAndEmeraldsToItems(Items.GUNPOWDER, 8, 4, HWGItems.BULLETS.get(), 48, 16, 50, 0.05F)},
+                5, new VillagerTrades.ItemListing[]{
+                        new VillagerTrades.ItemsAndEmeraldsToItems(Items.IRON_INGOT, 18, 8, HWGItems.ROCKETLAUNCHER.get(), 1, 16, 60, 0.05F),
+                        new VillagerTrades.ItemsAndEmeraldsToItems(Items.IRON_INGOT, 18, 8, HWGItems.G_LAUNCHER.get(), 1, 16, 60, 0.05F),
+                        new VillagerTrades.ItemsAndEmeraldsToItems(Items.IRON_INGOT, 18, 8, HWGItems.SNIPER.get(), 1, 16, 60, 0.05F)}
+        )));
     }
 
     public static Int2ObjectMap<VillagerTrades.ItemListing[]> copyToFastUtilMap(ImmutableMap<Integer, VillagerTrades.ItemListing[]> immutableMap) {
